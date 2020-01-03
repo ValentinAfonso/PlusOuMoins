763b7f58af316e8c1fae188a15aeb97df82c9dd7
ValentinAfonso <56429889+ValentinAfonso@users.noreply.github.com>
2020-01-03 16:55:02 +0100

    Add files via upload

---
+++ test/src/test/PlusOuMoins.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package test;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author valentin
+ *
+ */
+public class PlusOuMoins {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		Random nb = new Random();
+		int x=0;
+		x = nb.nextInt(101);
+		int solution;
+		int restart=0;
+		String recommencer;
+		
+		
+		do {
+			do {
+				System.out.println("Entrez une proposition : ");
+				solution = scan.nextInt();
+			
+			
+				if(solution == x) {
+					System.out.println("Vous avez gagné !!!!");
+					scan.nextLine();
+					System.out.println("Voulez vous recommencer ? (oui ou non)");
+					recommencer = scan.nextLine();
+					if(recommencer.equals("oui")) {
+						restart =1;
+						x = nb.nextInt(101);
+					}else if(recommencer.equals("non")) {
+						System.out.println("Au revoir !");
+						restart =0;
+					}else {
+						System.out.println("Veuillez entrer oui ou non");
+					}
+				}else if(solution < x){
+					System.out.println("Trop petit");
+				}else if(solution > x){
+					System.out.println("Trop grand");
+				}
+			}while(solution != x);
+		}while(restart ==1);
+
+	}
+
+}
